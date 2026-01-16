f0bf6ce92d467a207da8654bc4f82590e45721e2
JJY <Junyeong0721>
2026-01-16 11:08:33 +0900

    style: 주석처리

--- src/main/java/kr/soft/login/mapper/AuthMapper.java
+++ src/main/java/kr/soft/login/mapper/AuthMapper.java
@@ -6,6 +6,17 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface AuthMapper {
+
+    /**
+     * 로그인 프로세스
+     * @param param
+     * @return
+     */
     MemberLoginDTO login(String param);
+
+    /**
+     * 회원가입 프로세스
+     * @param registerDTO
+     */
     void register(RegisterDTO registerDTO);
 }
