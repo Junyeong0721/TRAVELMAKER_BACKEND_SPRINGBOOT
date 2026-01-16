8b1ca337691cc3cf820ee4e7b54a77c6e0383d63
JJY <Junyeong0721>
2026-01-16 11:16:14 +0900

    style: 주석 변경

--- src/main/java/kr/soft/login/mapper/AuthMapper.java
+++ src/main/java/kr/soft/login/mapper/AuthMapper.java
@@ -9,8 +9,16 @@ public interface AuthMapper {
 
     /**
      * 로그인 프로세스
+     * 작성일: 26-01-06
+     * 작업자: 명희승
      * @param param
-     * @return
+     * @return MemberLoginDTO
+     *     Long idx;           // pk
+     *     String userId;      // id
+     *     String userPw;      // 비밀번호
+     *     String nickname;    // 닉네임
+     *     String mbti;        // mbti
+     *     String title;       // title
      */
     MemberLoginDTO login(String param);
 
